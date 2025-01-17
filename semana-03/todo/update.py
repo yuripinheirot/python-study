80d6110d9304675444b38683042af264c556937c
Yuri Pinheiro <yuripinheirot@gmail.com>
2025-01-17 20:26:18 -0300

    chore: create semana-03 folder

---
+++ semana-03/todo/update.py
@@ -0,0 +1,19 @@
+import json
+from file_manager import read_file,write_file
+
+tasks: list = read_file()
+
+def update_task(task_id: str, data: str):
+    updated_tasks = []
+
+    for task in tasks:
+        if task.get("id") == task_id:
+            parsed_updated_task = json.loads(data)
+            parsed_updated_task["id"] = task["id"]
+            updated_tasks.append(parsed_updated_task)
+        else:
+            updated_tasks.append(task)
+
+    write_file(updated_tasks)
+    print(updated_tasks)
+    print("Task deleted successfully!")
